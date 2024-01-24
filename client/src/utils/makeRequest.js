49df1f3927dc99ec641949c00555b93c85be8d3b
golam mostafa <gmnayeem25@gmail.com>
2024-01-25 05:00:53 +0600

    fix problem

--- client/src/utils/makeRequest.js
+++ client/src/utils/makeRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:4200/api';
+const baseUrl = import.meta.env.VITE_API_URL;
 
 export const userRequest = axios.create({
     baseURL: baseUrl,
